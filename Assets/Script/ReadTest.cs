e3a8ebdb862d1538fd14087a90359f5260ac6573
ggodol <gghost10@naver.com>
2016-03-07 19:24:12 +0900

    commit

---
+++ Assets/Script/ReadTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReadTest : MonoBehaviour {
+
+    void Start()
+    {
+        // 저장되어있는 XML 파일을 읽어온다.
+        List<Word> WordList = WordIO.Read(Application.dataPath + "/Output/Items.xml");
+
+        // 결과물 출력.
+        for (int i = 0; i < WordList.Count; ++i)
+        {
+            Word word = WordList[i];
+            Debug.Log(string.Format("word[{0}] : ({1}, {2})",
+                i, word.id, word.value));
+        }
+    }
+}
---
+++ Assets/Script/WordIO.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System;
+
+//영어단어 자료 클래스
+public class Word
+{
+    public int id; //영어단어의 구분자
+    public string value; //영어 단어
+}
+
+public class WordIO
+{
+    //Xml파일을 읽어서 Word클래스의 List로 반환하는 메소드
+    public static List<Word> Read(string filePath)
+    {
+        XmlDocument Document = new XmlDocument();
+        Document.Load(filePath);
+        XmlElement WordListElement = Document["MyTest"];
+
+        List<Word> WordList = new List<Word>();
+
+        foreach (XmlElement WordElement in WordListElement.ChildNodes)
+        {
+            Word word = new Word();
+            word.id = Convert.ToInt32(WordElement.GetAttribute("id"));
+            word.value = WordElement.GetAttribute("value");
+            WordList.Add(word);
+        }
+        return WordList;
+    }
+}
